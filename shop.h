524f4b87e6e5aef4dce63e32c8af9fc30c67a388
Анастасія Варяниця <anastasia.00nastya00@gmail.com>
2026-02-26 20:29:56 +0200

    create base shop

---
+++ shop.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "iostream"
+#include "fstream"
+using namespace std;
+
+class shop{
+    protected:
+    int discount;
+    public:
+    shop(int d) {
+        discount =d;
+    }
+    virtual void print() =0; 
+};
